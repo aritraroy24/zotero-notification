2ee684a533fa84dc63a6976010958994702588fe
aritraroy24 <aritraroy24@gmail.com>
2023-12-25 16:58:06 +0000

    email problem fixed using SendGrid, Twig template added, hosted

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include('smtp/PHPMailerAutoload.php');
 require 'vendor/autoload.php';
+
 header('Content-type: text/plain');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -19,54 +19,72 @@ $groupApiUrl = 'https://api.zotero.org/users/' . $zoteroUserId . '/groups?key=' 
 $groupsDataResponse = file_get_contents($groupApiUrl);
 $groupsDataArray = json_decode($groupsDataResponse, true);
 
-// function smtp_mailer($to, $subject, $msg)
-// {
-//     $GMAIL_APP_PASSWORD = $_ENV['GMAIL_APP_PASSWORD'];
-//     $FROM_EMAIL = $_ENV['FROM_EMAIL'];
-//     $mail = new PHPMailer();
-//     $mail->IsSMTP();
-//     $mail->SMTPAuth = true;
-//     $mail->SMTPSecure = 'tls';
-//     $mail->Host = "smtp.gmail.com";
-//     $mail->Port = 587;
-//     $mail->IsHTML(true);
-//     $mail->CharSet = 'UTF-8';
-//     //$mail->SMTPDebug = 2; 
-//     $mail->Username = $FROM_EMAIL;
-//     $mail->GMAIL_APP_PASSWORD = $GMAIL_APP_PASSWORD;
-//     $mail->SetFrom($FROM_EMAIL);
-//     $mail->Subject = $subject;
-//     $mail->Body = $msg;
-//     $mail->AddAddress($to);
-//     $mail->SMTPOptions = array('ssl' => array(
-//         'verify_peer' => false,
-//         'verify_peer_name' => false,
-//         'allow_self_signed' => false
-//     ));
-//     if (!$mail->Send()) {
-//         echo $mail->ErrorInfo;
-//     } else {
-//         return 'Sent';
-//     }
-// }
-
-function writeFileAndCheck($folderName, $itemId, $itemData, $itemType, $groupName)
+function smtp_mailer($group_name, $id, $type, $title, $message)
 {
-    $folderPath = $groupName . "/" . $folderName;
+
+    $SENDGRID_API_KEY = $_ENV['SENDGRID_API_KEY'];
+    $FROM_EMAIL = $_ENV['FROM_EMAIL'];
+    $TO_EMAIL = $_ENV['TO_EMAIL'];
+    $sub = "New item has been added to your Zotero Group";
+
     try {
-        if (!is_dir($folderPath)) {
-            mkdir($folderPath, 0755, true);
-        }
-    } catch (\Throwable $th) {
-        echo $th;
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/');
+        $twig = new \Twig\Environment($loader);
+
+        $template = $twig->load('email_template.twig');
+
+    } catch (Exception $e) {
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
     }
-    $filePath = $itemId . '.txt';
-    if (!file_exists($filePath)) {
-        file_put_contents($folderPath . $filePath, $itemData);
-        // SENDING EMAIL
-        // $TO_EMAIL = $_ENV['TO_EMAIL'];
-        // $sub = "New item(s) has(have) been added to your Zotero Group";
-        // smtp_mailer($TO_EMAIL, $sub, 'Hello World');
+
+    $emailBody = $template->render([
+        'group_name' => $group_name,
+        'id' => $id,
+        'type' => $type,
+        'title' => $title,
+        'message' => $message
+    ]);
+
+
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom($FROM_EMAIL, "Zotero Notification");
+    $email->setSubject($sub);
+    $email->addTo($TO_EMAIL, "Aritra Roy");
+    $email->addContent(
+        "text/html", $emailBody
+    );
+
+    $sendgrid = new \SendGrid($SENDGRID_API_KEY);
+
+    try {
+        $sendgrid->send($email);
+    } catch (Exception $e) {
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
+    }
+}
+
+function writeFileAndCheck($folderName, $itemId, $itemData, $itemType, $groupName, $titleData, $userID, $message = null)
+{
+    $myZoteroUserId = $_ENV["ZOTERO_USERID"];
+    if ($myZoteroUserId != $userID) {
+        $folderPath = 'zotero-groups/' . $groupName . "/" . $folderName;
+        try {
+            if (!is_dir($folderPath)) {
+                mkdir($folderPath, 0755, true);
+            }
+        } catch (\Throwable $th) {
+            echo $th;
+        }
+        $filePath = $folderPath . '/' . $itemId . '.txt';
+        if (!file_exists($filePath)) {
+            file_put_contents($filePath, $itemData);
+            echo "I'll send an email for item " . $itemId . ' - ' . $itemType . ' - ' . $titleData . "\n";
+            if ($message === null) {
+                smtp_mailer($groupName, $itemId, $itemType, $titleData, "Not Applicable");
+            } else {
+                smtp_mailer($groupName, $itemId, $itemType, $titleData, $message);
+            }
+        }
     }
 }
 
@@ -85,31 +103,35 @@ if ($groupsDataArray !== null) {
             foreach ($itemsDataArray as $dataItem) {
                 $dataItemKey = $dataItem["key"];
                 $dataItemType = $dataItem['data']['itemType'];
+                $userId = $dataItem["meta"]["createdByUser"]["id"];
                 if ($dataItemType === "journalArticle") {
                     $journalArticles = "Item Key: " . $dataItemKey . "\n" . "Type: Journal Article" . "\n" . "Title: " . $dataItem["data"]["title"];
-                    writeFileAndCheck("journal_article/", $dataItemKey, $journalArticles, "journalArticle", $groupName);
+                    writeFileAndCheck("journal_article/", $dataItemKey, $journalArticles, "Journal Article", $groupName, $dataItem["data"]["title"], $userId);
                 } elseif ($dataItemType === "note") {
                     $standAloneNote = "Item Key: " . $dataItemKey . "\n" . "Type: Stand Alone Note";
-                    writeFileAndCheck("stand_alone_note/", $dataItemKey, $standAloneNote, "note", $groupName);
+                    writeFileAndCheck("stand_alone_note/", $dataItemKey, $standAloneNote, "Stand Alone Note", $groupName, "Not Applicable [Stand Alone Note]", $userId);
                 } else {
                     $parentItemId = $dataItem['data']['parentItem'];
                     $parentItemUrl = 'https://api.zotero.org/groups/' . $item . '/items/' . $parentItemId . '?key=' . $zoteroApi;
                     $parentItemTitle = json_decode(file_get_contents($parentItemUrl), true)['data']['title'];
                     if ($dataItemType === "note") {
                         $note = "Item Key: " . $dataItemKey . "\n" . "Type: Note" . "\n" . "Title: " . $parentItemTitle;
-                        writeFileAndCheck("notes/", $dataItemKey, $note, "note", $groupName);
+                        writeFileAndCheck("notes/", $dataItemKey, $note, "Note", $groupName, $parentItemTitle . " [Parent Element]", $userId);
                     } elseif ($dataItemType === "attachment") {
                         $attachment = "Item Key: " . $dataItemKey . "\n" . "Type: Attachment" . "\n" . "Title: " . $parentItemTitle;
-                        writeFileAndCheck("attachments/", $dataItemKey, $attachment, "attachment", $groupName);
+                        writeFileAndCheck("attachments/", $dataItemKey, $attachment, "Attachment", $groupName, $parentItemTitle . ' [Parent Element]', $userId);
                     } elseif ($dataItemType === "book") {
                         $book = "Item Key: " . $dataItemKey . "\n" . "Type: Book" . "\n" . "Title: " . $parentItemTitle;
-                        writeFileAndCheck("books/" . $dataItemType . "/", $dataItemKey, $book, "book", $groupName);
+                        writeFileAndCheck("books/" . $dataItemType . "/", $dataItemKey, $book, "Book", $groupName, $parentItemTitle . ' [Parent Element]', $userId);
                     } elseif ($dataItem['data']['annotationComment'] !== "") {
                         $annotationComments = "Item Key: " . $dataItemKey . "\n" . "Type: Annotation Comment" . "\n" . "Title: " . $parentItemTitle . "\n" . "Comment Details: " . $dataItem['data']['annotationComment'];
-                        writeFileAndCheck("comments/", $dataItemKey, $annotationComments, "annotationComment", $groupName);
+                        writeFileAndCheck("comments/", $dataItemKey, $annotationComments, "Annotation Comment", $groupName, $parentItemTitle . " [Parent Element]", $userId, $dataItem['data']['annotationComment']);
+                    } elseif ($dataItemType === "annotation") {
+                        $annotation = "Item Key: " . $dataItemKey . "\n" . "Type: Annotation" . "\n" . "Annotation Text: " . $dataItem['data']['annotationText'];
+                        writeFileAndCheck("annotations/", $dataItemKey, $annotation, "Annotation Text", $groupName, $parentItemTitle . ' [Parent Element]', $userId, $dataItem['data']['annotationText']);
                     } else {
                         $othersData = "Item Key: " . $dataItemKey . "\n" . "Type:" . $dataItemType;
-                        writeFileAndCheck("others/", $dataItemKey, $othersData, $dataItemType, $groupName);
+                        writeFileAndCheck("others/" . $dataItemType . "/", $dataItemKey, $othersData, $dataItemType, $groupName, "Not Applicable", $userId);
                     }
                 }
             }
